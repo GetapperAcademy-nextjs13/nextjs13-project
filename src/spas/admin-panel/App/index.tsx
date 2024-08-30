1884c72750e8e63159bf8a260396c303efe9deb7
Aramis Todesco <todesco.aramis@gmail.com>
2024-08-30 20:54:31 +0200

    09 - Introduzione alle Single Page Applications

---
+++ src/spas/admin-panel/App/index.tsx
@@ -0,0 +1,46 @@
+import React, { memo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { AppSnackbar } from "@/components/AppSnackbar";
+import useAppHooks from "./index.hooks";
+import { DashboardScene, LoginScene } from "@/spas/admin-panel/scenes";
+import { Outlet } from "react-router";
+
+const App: React.FC = () => {
+  const { theme, open, type, message, handleClose } = useAppHooks();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter basename="/admin-panel">
+        <Routes>
+          <Route path="/" element={<span>TEST</span>} />
+          <Route path="/login" element={<LoginScene />} />
+          <Route path="/dashboard" element={<DashboardScene />}>
+            <Route
+              path="users"
+              element={
+                <span>
+                  Users <Outlet />
+                </span>
+              }
+            >
+              <Route path="about" element={<span>About</span>} />
+              <Route path="contacts" element={<span>Contacts</span>} />
+              <Route path="*" element={<span>All</span>} />
+            </Route>
+            <Route path="posts" element={<span>Posts</span>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <AppSnackbar
+        open={open}
+        message={message}
+        severity={type}
+        onClose={handleClose}
+      />
+    </ThemeProvider>
+  );
+};
+
+export default memo(App);
---
+++ src/spas/admin-panel/scenes/DashboardScene/index.tsx
@@ -0,0 +1,22 @@
+import React, { memo } from "react";
+import { useDashboardScene } from "./index.hooks";
+import { Box, Paper } from "@mui/material";
+import { Outlet } from "react-router";
+
+type DashboardSceneProps = {};
+
+export const DashboardScene = memo(({}: DashboardSceneProps) => {
+  const {} = useDashboardScene();
+
+  return (
+    <Box sx={{ p: 10, bgcolor: "#f00" }}>
+      Dashboard:
+      <Paper>
+        Sottoroute:
+        <Outlet />
+      </Paper>
+    </Box>
+  );
+});
+
+DashboardScene.displayName = "DashboardScene";
